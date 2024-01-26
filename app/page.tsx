3b4c76f4691249dbe7ae9ebaf3ec156629898041
Konstantin Nikolaev <github@nikolaev.us>
2024-01-25 19:40:28 -0800

    Dark/Light Theme Set Up

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/mode-toggle";
 
 export default function Home() {
   return (
-    <div>
+    <div className="">
       <Button>Check </Button>
+      <ModeToggle />
     </div>
   );
 }
