5e7edf7f92f4b6aa2cfc7ce2a4d58f225c5e00ec
Konstantin Nikolaev <github@nikolaev.us>
2024-03-08 20:23:43 -0800

    Delete Data Dump

--- pscale_dump_your-wallet_main_20240307_204054/your-wallet.Budget.00001.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `Budget`(`id`,`userId`,`budgetName`,`createdAt`,`updatedAt`,`currencyId`) VALUES
-("80c4115d-eb71-4634-b540-72075f8b7ddd","user_2c9N81xBJVwGAgJm7K3mnTADFqQ","Ежемесячный ","2024-02-10 05:05:52.459","2024-02-10 05:05:52.459","c27c5044-29b6-4811-9143-beb3bfb9bd8a");
--- pscale_dump_your-wallet_main_20240307_204054/your-wallet.Currency-schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `Currency` (
-  `id` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `code` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `symbol` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
